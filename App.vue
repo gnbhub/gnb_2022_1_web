745739f7e1a5b72229749c310b9e632a820b9b3b
wdh4804 <wdh48040@gmail.com>
2022-05-12 19:59:56 +0900

    week4

---
+++ App.vue
@@ -0,0 +1,120 @@
+<template>
+
+<div class="black-bg" v-if="모델창열렸니 == true">
+  <div class="white-bg">
+    <h4>상세페이지임</h4>
+    <p>상세페이지 내용임</p>
+    <button @click="모델창열렸니 = false">닫기</button>
+  </div>
+</div>
+<div>
+  <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+ 
+</div>
+
+
+
+
+
+<div>
+  <img src="./assets/room0.jpg">
+  <button @click="모델창열렸니 = true">{{ 원룸들[0] }} </button>
+  <p>{{ 원룸들[0] }}</p>
+  <button @click="신고수[0]++">
+    허위매물신고
+  </button>
+  <span>
+    신고수 : {{신고수[0]}}
+  </span>
+</div>
+
+<div>
+    <img src="./assets/room1.jpg">
+  <button>{{ products[1] }} </button>
+  <p>60만원</p>
+  <button @click="신고수[1]++">
+    허위매물신고
+  </button>
+  <span>
+    신고수 : {{신고수[1]}}
+  </span>
+</div>
+
+<div>
+    <img src="./assets/room2.jpg">
+  <button>{{ products[2] }} </button>
+  <p>70만원</p>
+  <button @click="신고수[2]++">
+    허위매물신고
+  </button>
+  <span>
+    신고수 : {{신고수[2]}}
+  </span>
+</div>
+ 
+  
+</template>
+
+<script>
+
+import data from './one room.js';
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      원룸들 : data,
+      모델창열렸니 : false,
+      신고수 : [0,0,0],
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+
+      
+    }
+  },
+  
+ 
+}
+</script>
+
+<style>
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var http = require('http');
 
 //서버 생성 및 실행
 http.createServer(function(request, response){
-    
+    //html 파일 읽기
+    fs.readFile('러브캐쳐 메인페이지.html', function(error, data){
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end(data);
+    })
+/*
     //쿠키 입력
     var date = new Date();
     date.setDate(date.getDate()+7);
@@ -18,6 +23,7 @@ http.createServer(function(request, response){
     });
     //쿠키 출력
     response.end('<h1>'+request.headers.cookie+'</h1>');
+*/
 }).listen(24554, function(){
     console.log('Server Running at http://127.0.0.1:24554');
 });
